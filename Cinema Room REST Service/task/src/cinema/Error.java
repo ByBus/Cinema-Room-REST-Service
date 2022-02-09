459f21e4c90b23e626b3fa316549150684eaf8ad
Alex <al3xxxgm@gmail.com>
2022-02-10 02:57:14 +0300

    small refactoring

--- Cinema Room REST Service/task/src/cinema/Error.java
+++ Cinema Room REST Service/task/src/cinema/Error.java
@@ -3,7 +3,6 @@ package cinema;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
--- Cinema Room REST Service/task/src/cinema/Seat.java
+++ Cinema Room REST Service/task/src/cinema/Seat.java
@@ -2,19 +2,15 @@ package cinema;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class Seat {
     private final int row;
     private final int column;
     private final int price;
     private boolean isPurchased = false;
-
-    public Seat(int row, int column, int price) {
-        this.row = row;
-        this.column = column;
-        this.price = price;
-    }
 
     @JsonIgnore
     public boolean isPurchased() {
